c701fcf73a5a3eb47c198879cefd1f1736d724d7
kame_mac <kametyaso@gmail.com>
2024-12-15 19:41:43 +0900

    del: dbSeed from entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,6 +3,6 @@ set -e
 
 rm -f /app/tmp/pids/server.pid
 bundle exec rails db:migrate RAILS_ENV=production
-bundle exec rails db:seed RAILS_ENV=production
+# bundle exec rails db:seed RAILS_ENV=production
 
 exec "$@"
