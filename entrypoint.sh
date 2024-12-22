aac7a0fba0b88cfeaab5b77902958242fe76c033
kame_mac <kametyaso@gmail.com>
2024-12-22 17:45:42 +0900

    fix: entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,11 @@
 set -e
 
 rm -f /app/tmp/pids/server.pid
-bundle exec rails db:migrate RAILS_ENV=production
+if [ "$RAILS_ENV" = "production" ]; then
+bundle exec rails db:migrate
+# bundle exec rails assets:precompile
+# bundle exec rails assets:clean
 # bundle exec rails db:seed RAILS_ENV=production
+fi
 
 exec "$@"
