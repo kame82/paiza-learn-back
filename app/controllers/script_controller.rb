6f3f3cac2643d7e77f3e41aea1a863f67cd70014
kame_mac <kametyaso@gmail.com>
2024-12-22 22:28:47 +0900

    fix:script

--- app/controllers/script_controller.rb
+++ app/controllers/script_controller.rb
@@ -2,7 +2,6 @@ class ScriptController < ApplicationController
   def execute
     # language = params[:language]
     code = params[:code]
-    # puts code
     result = execute_ruby
 
     render json: { output: result }
@@ -11,28 +10,7 @@ class ScriptController < ApplicationController
 
   private
   # DockerコンテナでRubyコードを実行する
-  # def execute_ruby(code)
   def execute_ruby
-    # `docker run --rm -v $(pwd):/app ruby:3.3.4 ruby -e "#{code}"`
-        # 一時ファイルを作成してコードを実行
-        temp_dir = '/tmp/code_execution'
-        FileUtils.mkdir_p(temp_dir)
-
-        # 実行するコードをファイルに書き込む
-        # File.write("#{temp_dir}/script.rb", code)
-        File.write("#{temp_dir}/script.rb", "puts 'hello code'")
-
-        # Dockerコンテナでコードを実行
-        `docker run --rm \
-          --memory=50m \
-          --memory-swap=50m \
-          --cpus=0.5 \
-          --network none \
-          -v #{temp_dir}:/code \
-          ruby:3.3.4 \
-          timeout 5 ruby /code/script.rb`
-  ensure
-        # 一時ファイルを削除
-        FileUtils.rm_rf(temp_dir)
+    return puts "execute_ruby"
   end
 end
